324ec3bbaafeee33d846b037ca03c9bd15d68063
unknown <7f8g4ky@code.edu.az>
2022-11-02 14:36:04 +0400

    task

---
+++ task1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 324, 23, 43, 123, 24 };
+            int c;
+            int sum = 0;
+            int say = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                c = array[i];
+                
+                while (array[i] > 0)
+                {
+                    array[i] /= 10;
+                    say++;
+
+                }
+
+                if (say > 2)
+                {
+
+                    sum += c;
+
+                }
+                say = 0;
+            }
+            Console.WriteLine(sum);
+        }
+
+    }
+}
+
+
---
+++ task2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sum = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] array = { 1,2,4,1,1,6,0,0,9 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    if (array[i] == c)
+                    {
+                        sum[c]++;
+                        break;
+                    }
+                }
+
+            }
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine(x+"="+sum[x]);
+            }
+
+
+
+        }
+    }
+}
